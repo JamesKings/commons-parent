cf039328cba7a5bb1a21f600a864353111952357
caoheng <caoheng@jiuhuar.com>
2019-11-17 18:01:39 +0800

    add commons-utils

---
+++ commons-utils/src/main/java/cn/smdq/ch/commons/utils/TestUtils.java
@@ -0,0 +1,13 @@
+package cn.smdq.ch.commons.utils;
+
+/**
+ * @author shuimodanqing
+ * create at:  2019/11/17  5:13 PM
+ * @description
+ */
+public class TestUtils {
+
+    public String test() {
+        return "Hello,World";
+    }
+}
